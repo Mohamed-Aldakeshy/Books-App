b0975d9bbda0d5585f9900b037c3d370ecc86129
mohamed <mohamedhassouna88@gmail.com>
2023-10-05 14:36:05 -0700

    add custom book item to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:books_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:books_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,11 +7,16 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
-        children: const [
-          CustomBookDetailsAppBar(),
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            child: const CustomBookImage(),
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:books_app/constants.dart';
 import 'package:books_app/core/utils/app_router.dart';
-import 'package:books_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const BooklyApp());
