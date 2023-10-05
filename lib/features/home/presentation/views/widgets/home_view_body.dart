41ea5a655b2623ed541be5deccd062cae1a4001a
mohamed <mohamedhassouna88@gmail.com>
2023-10-04 17:58:20 -0700

    best seller item & adding text styles class

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:books_app/core/utils/assets.dart';
 import 'package:books_app/core/utils/styles.dart';
 import 'package:books_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:books_app/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -20,10 +21,60 @@ class HomeViewBody extends StatelessWidget {
           padding: const EdgeInsets.only(left: 24),
           child: Text(
             'Best Seller',
-            style: Style.titleMedium,
+            style: Style.textStyle18,
           ),
-        )
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  style: Style.textStyle20,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
