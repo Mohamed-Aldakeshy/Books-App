f6ac18f7d4c16769df1087220413ca2b9e912ff1
mohamed <mohamedhassouna88@gmail.com>
2023-10-07 02:46:56 -0700

    create feautered books method

--- lib/features/home/presentation/manager/feautered_books_cubit.dart/feautered_books_cubit.dart
+++ lib/features/home/presentation/manager/feautered_books_cubit.dart/feautered_books_cubit.dart
@@ -1,6 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:books_app/features/home/data/repos/home_repo.dart';
 import 'package:books_app/features/home/presentation/manager/feautered_books_cubit.dart/feautered_books_state.dart';
 
 class FeauteredBooksCubit extends Cubit<FeauteredBooksState> {
-  FeauteredBooksCubit() : super(FeauteredBooksInitial());
+  FeauteredBooksCubit(this.homeRepo) : super(FeauteredBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeauteredBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+        (failure) => {
+              emit(FeauteredBooksFailure(failure.errMessage)),
+            },
+        (books) => {emit(FeauteredBooksSuccess(books))});
+  }
 }
